22e83f016c8cf453ea269ecb30b7182a9db7a422
weiweitoo <sengweitoo@gmail.com>
2018-08-11 16:50:13 +0800

    Add player movement script. and add some emprt folder

---
+++ Assets/2-Scripts/PlayerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float m_movingSpeed = 1;
+	public float x = 0;
+	public float z = 0;
+
+	private CharacterController m_CharacterController;
+	private Vector3 m_MoveDir = Vector3.zero;
+
+	public int right = 0;
+	public int left = 0;
+
+	void Start () {
+		m_CharacterController = GetComponent<CharacterController>();
+	}
+	
+	void FixedUpdate () {
+		m_MoveDir = new Vector3(x,0,z) * m_movingSpeed;
+		m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+	}
+
+	void Update(){
+		if(right == 1){
+			TurnRight();
+		}
+		else if(left == 1){
+			TurnLeft();
+		}
+	}
+
+	public void TurnRight(){
+		//fuck it
+		if(x == 0 && z == m_movingSpeed){
+			x = m_movingSpeed;
+			z = 0;
+		}
+		else if (x == m_movingSpeed && z == 0){
+			x = 0;
+			z = -m_movingSpeed;
+		}
+		else if(x == -m_movingSpeed && z == 0){
+			x = 0;
+			z = m_movingSpeed;
+		}
+		else if(x == 0 && z == -m_movingSpeed){
+			x = -m_movingSpeed;
+			z = 0;
+		}
+		right = 0;
+	}
+
+	public void TurnLeft(){
+		//fuck it
+		if(x == 0 && z == m_movingSpeed){
+			x = -m_movingSpeed;
+			z = 0;
+		}
+		else if (x == m_movingSpeed && z == 0){
+			x = 0;
+			z = m_movingSpeed;
+		}
+		else if(x == -m_movingSpeed && z == 0){
+			x = 0;
+			z = -m_movingSpeed;
+		}
+		else if(x == 0 && z == -m_movingSpeed){
+			x = m_movingSpeed;
+			z = 0;
+		}
+		left = 0;
+	}
+
+	//TODO shake camera
+}
